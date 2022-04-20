31227576b28d99381b8d119ee9810581b1577560
ViniciusAntonello <antonello.luis@gmail.com>
2022-04-20 19:38:43 -0300

    DELETE and GET functions

--- controllers/cursos.controller.js
+++ controllers/cursos.controller.js
@@ -1,24 +1,49 @@
-const listaDeCursos = require('../models/cursos.json');
+let listaDeCursos = require('../models/cursos.json');
 const fs = require('fs');
 
 const CursoController = {
     cadastrarCurso(req, res){
         const { titulo, descricao, professor } = req.body;
 
+        // Parametro vazio é undefined, considerado falsy.
         if(!titulo || !descricao || !professor){
             return res.status(400).json({error: 'Você precisa passar os atributos corretamente.'})
         }
 
         listaDeCursos.push({
+            id: listaDeCursos.length,
             titulo,
             descricao,
             professor,
         });
 
-        fs.writeFileSync('../models/cursos.json', JSON.stringify(listaDeCursos));
+        fs.writeFileSync('./models/cursos.json', JSON.stringify(listaDeCursos));
 
-        res.status(201).json({message: 'Cadastro efetuado com sucesso!'})
+        res.status(201).json({message: 'Cadastro efetuado com sucesso!'});
     },
+
+    listarCursos(req, res){
+
+        const listaDeCursos = fs.readFileSync("./models/cursos.json", 'utf-8');
+
+        res.status(200).send(listaDeCursos);
+    },
+
+    deletarCurso(req, res) {
+        const { id } = req.params;
+        let novaLista = [];
+
+
+        listaDeCursos.forEach(curso => {
+            if (curso.id != id) {
+                novaLista.push(curso);
+            };
+        });
+
+        fs.writeFileSync('./models/cursos.json', JSON.stringify(novaLista));
+
+        res.sendStatus(204);
+    }
 };
 
 module.exports = CursoController;
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const CursoController = require('../controllers/cursos.controller');
 const routes = express.Router();
 
 routes.post('/cursos', CursoController.cadastrarCurso);
+routes.get('/CURSOS', CursoController.listarCursos);
+routes.delete('/cursos/:id', CursoController.deletarCurso);
 
 module.exports = routes;
